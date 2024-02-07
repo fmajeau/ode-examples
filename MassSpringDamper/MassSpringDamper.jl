283936f74a9731c45859a43af45cb2d5077cadd3
fmajeau <52875759+fmajeau@users.noreply.github.com>
2024-02-07 15:21:14 -0700

    Add comments no functional changes

--- MassSpringDamper/MassSpringDamper.jl
+++ MassSpringDamper/MassSpringDamper.jl
@@ -5,29 +5,30 @@ Pkg.activate(".") # activate the environment defined by the toml files in this d
 Pkg.instantiate() # install missing dependencies, make sure environment is ready to use
 
 # Import packages
-using DifferentialEquations # ODEProblem, solve
-using Plots # plot
+using DifferentialEquations # ODEProblem(), solve()
+using Plots                 # plot()
 
 # Define parameters
 m = 20  # [kg]
 k = 15  # [kg-m/s] spring constant
-b = 2  # [kg-m] damper constant
-F = 1  # [kg-m/s^2] magnitude of constant external force
-p = [m, k, b, F]
+b = 2   # [kg-m] damper constant
+F = 1   # [kg-m/s^2] magnitude of constant external force
+p = [m, k, b, F] # NOTE: passed into ODEProblem; order persists in the ODEFunction
 
 # Define time range to solve
 t_start = 0  # [s]
 t_end = 100  # [s]
-tspan = (t_start, t_end) # [s]
+tspan = (t_start, t_end) # NOTE: passed into ODEProblem
 
 # Define initial conditions
-x0 = 2 # [m]
+x0 = 2  # [m]
 v0 = 3  # [m/s]
-u0 = [x0, v0]
+u0 = [x0, v0] # NOTE: passed into ODEProblem; order persists in the ODEFunction and ODESolution
 
-# Define the function that stores our  system of equations.
-# It will be called within every iteration of the numerical method performed by "solve"
-function mass_spring_damper!(du,u,p,t)
+# Define the function that stores our system of equations.
+# - It must be constructed as an ODEFunction Type
+# - It will be called within every iteration of the numerical method performed by solve()
+function mass_spring_damper!(du, u, p, t)
 
     # Rename states for clarity
     x = u[1]  # [m]
@@ -40,8 +41,10 @@ function mass_spring_damper!(du,u,p,t)
     F = p[4]  # [kg-m/s^2]
 
     # Define the differential equations
-    du[1] = v
-    du[2] = -((k/m)*x) - ((b/m)*v) + ((1/m)*F)
+    #  u[1] = x, so du[1] = dx/dt
+    #  u[2] = v, so du[2] = dv/dt
+    du[1] = v                                   
+    du[2] = -((k/m)*x) - ((b/m)*v) + ((1/m)*F)  
 
     # Print to get a sense of how this function is used by solve!
     print("[time $t] du:")
@@ -49,14 +52,16 @@ function mass_spring_damper!(du,u,p,t)
 
 end
 
-# Build problems
+# Build the ODE problem
+# Return Type: ODEProblem
 prob = ODEProblem(mass_spring_damper!, u0, tspan, p)
 
-# Solve the system
+# Solve the ODE problem by specifying a numerical method
+# Return Type: ODESolution
 solution = solve(prob, RK4())
 
 # Plot the time-domain trajectory of all state variables on the same set of axes
-plot(solution, 
+plot(solution,  # NOTE: plot() automatically interprets/plots the ODESolution Type
     title="Time Domain: All states variables on one set of axes", 
     label=["x" "v"]
     )
@@ -73,8 +78,9 @@ plot(solution,
 
 # Plot the phase portrait of the two state variables
 plot(solution, 
-    idxs=(1,2), 
+    idxs=(1,2),  # 0 = time, 1 = first state variable, 2 = second state variable, ...
     title="Phase Portrait: Velocity vs Position", 
     xlabel="Position [m]", 
     ylabel="Velocity [m/s]"
     )
+    
